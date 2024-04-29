e4dadf7ac1923f1cb79dfeec4c3530c2a90f4384
juliamaria29 <105889635+juliamaria29@users.noreply.github.com>
2024-04-29 19:38:55 -0300

    fgt

--- main.js
+++ main.js
@@ -43,9 +43,9 @@ function calculaTempo(tempoObjetivo) {
 
 function atualizaCronometro(){
     document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[2])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[3])[3];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
 
     for (let i=0; i<contadores.length;i++){
        // contadores[i].textContent = calculaTempo(tempos[i]);   
